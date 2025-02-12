5e8f8ad85b61dc2ab7d15f0753c6f6ba9c3d5eb1
ali3amer <aliaamer549@gmail.com>
2025-02-12 20:32:38 +0200

    last update night 12-02-2025

--- app/Livewire/Settings.php
+++ app/Livewire/Settings.php
@@ -52,117 +52,199 @@ class Settings extends Component
     public function getFromExcel()
     {
         Artisan::call("migrate:fresh --seed");
+
         $filePath = public_path('center.xlsx');
 
         $excelSpreadSheetData = Excel::toCollection(null, $filePath);
-        // 15 النوع
-        // 16 رقم المدرب
-        // 17 اسم المدرب
-        //19 رقم البرنامج
-        //20 اسم البرنامج
-        // 23 نوع البرنامج
-        \App\Models\Trainer::where("id", "!=", 0)->delete();
-        for ($i = 1; $i <= 19; $i++) {
+
+        for ($i = 1; $i <= 20; $i++) {
             $row = $excelSpreadSheetData[0][$i];
+
             \App\Models\Trainer::create([
-                'id' => $row['16'],
-                'arabic_name' => $row['17'],
-                'gender' => $row['15'],
+                'id' => $row[1],
+                'arabic_name' => $row[10],
+                'gender' => $row[11],
                 'user_id' => auth()->id(),
             ]);
         }
 
-        \App\Models\Course::where("id", "!=", 0)->delete();
-        for ($i = 1; $i <= 34; $i++) {
+        for ($i = 1; $i <= 1283; $i++) {
             $row = $excelSpreadSheetData[0][$i];
-            \App\Models\Course::create([
-                'id' => $row['19'],
-                'arabic_name' => $row['20'],
-                'type' => $row['23'],
-                'user_id' => auth()->id(),
-            ]);
-        }
-
-        // 9 رقم المدرب
-        //10 رقم البرنامج
-        \App\Models\Batch::where("id", "!=", 0)->delete();
-        for ($i = 1; $i <= 1191; $i++) {
-            $row = $excelSpreadSheetData[0][$i];
-
-            $excelDate = $row[0];
-            if (!is_numeric($excelDate)) {
-                dd($row);
+            if ($row[0] != null) {
+                $excelDate = $row[0];
             }
-
             $unixTimestamp = ($excelDate - 25569) * 86400; // تحويل إلى الطابع الزمني
             $date = date('Y-m-d', $unixTimestamp);
 
-            $count = \App\Models\Batch::where('id', $row[11])->count();
+            if ($row[5] == 'ورشة') {
+                $type = 'workshop';
+            } elseif ($row[5] == 'دورة') {
+                $type = 'session';
+            } else {
+                $type = 'course';
+            }
+
+            $count = \App\Models\Course::where('arabic_name', $row[4])->count();
+
             if ($count == 0) {
-                \App\Models\Batch::create([
-                    'id' => $row[11],
-                    'trainer_id' => $row[9],
-                    'course_id' => $row[10],
+                $course = \App\Models\Course::create([
+                    'arabic_name' => $row[4],
+                    'type' => $type,
+                    'duration' => 'day',
+                    'duration_value' => 5,
+                    'user_id' => auth()->id(),
+                ]);
+            }
+
+            $count = \App\Models\Batch::where('course_id', $course->id)->where('start_date', $date)->count();
+            $trainer_id = \App\Models\Trainer::where('arabic_name', $row[6])->first();
+            if ($trainer_id == null) {
+                dd($row[1]);
+            }
+            if ($count == 0) {
+                $batch = \App\Models\Batch::create([
+                    'trainer_id' => $trainer_id->id,
+                    'course_id' => $course->id,
                     'start_date' => $date,
                     'end_date' => $date,
                     'completed' => true,
                     'user_id' => auth()->id(),
                 ]);
             }
-        }
-        $id = 1;
-        \App\Models\Student::where("id", "!=", 0)->delete();
-        for ($i = 1; $i <= 1191; $i++) {
-            $row = $excelSpreadSheetData[0][$i];
-
-            $excelDate = $row[0];
-
-            $unixTimestamp = ($excelDate - 25569) * 86400; // تحويل إلى الطابع الزمني
-            $date = date('Y-m-d', $unixTimestamp);
 
             $count = \App\Models\Student::where('arabic_name', $row[3])->count();
             if ($count == 0) {
-                \App\Models\Student::create([
-                    'id' => $id,
+                $student = \App\Models\Student::create([
+                    'id' => $row[1],
                     'arabic_name' => $row[3],
-                    'phone' => $row[4],
+                    'gender' => 'male',
                     'user_id' => auth()->id(),
                 ]);
-                $id++;
             }
-        }
 
-        $id = 1;
-        \App\Models\BatchStudent::where("id", "!=", 0)->delete();
-        for ($i = 1; $i <= 1191; $i++) {
-            $row = $excelSpreadSheetData[0][$i];
-
-            $excelDate = $row[0];
-
-            $unixTimestamp = ($excelDate - 25569) * 86400; // تحويل إلى الطابع الزمني
-            $date = date('Y-m-d', $unixTimestamp);
-
-            $student = \App\Models\Student::where('arabic_name', $row[3])->first();
-
-            if ($student) {
-                \App\Models\BatchStudent::create([
-                    'id' => $id,
-                    'student_number' => $row[1],
-                    'student_id' => $student->id,
-                    'batch_id' => $row[11],
-                    'want_certification' => $row[2] != null ? true : false,
-                    'certification_id' => $row[2] != null ? $row[2] : null,
-                    'date' => $date,
-                    'user_id' => auth()->id(),
-                ]);
-                $id++;
-
-            } else {
-                dd($row);
-            }
+            \App\Models\BatchStudent::create([
+                'student_number' => $row[1],
+                'student_id' => $student->id,
+                'batch_id' => $batch->id,
+                'want_certification' => $row[2] != 0 ? true : false,
+                'certification_id' => $row[2] != 0 ? $row[2] : null,
+                'date' => $date,
+                'user_id' => auth()->id(),
+            ]);
         }
 
 
+//        $filePath = public_path('center.xlsx');
+//
+//        $excelSpreadSheetData = Excel::toCollection(null, $filePath);
+//        // 15 النوع
+//        // 16 رقم المدرب
+//        // 17 اسم المدرب
+//        //19 رقم البرنامج
+//        //20 اسم البرنامج
+//        // 23 نوع البرنامج
+//        \App\Models\Trainer::where("id", "!=", 0)->delete();
+//        for ($i = 1; $i <= 19; $i++) {
+//            $row = $excelSpreadSheetData[0][$i];
+//            \App\Models\Trainer::create([
+//                'id' => $row['16'],
+//                'arabic_name' => $row['17'],
+//                'gender' => $row['15'],
+//                'user_id' => auth()->id(),
+//            ]);
+//        }
+//
+//        \App\Models\Course::where("id", "!=", 0)->delete();
+//        for ($i = 1; $i <= 34; $i++) {
+//            $row = $excelSpreadSheetData[0][$i];
+//            \App\Models\Course::create([
+//                'id' => $row['19'],
+//                'arabic_name' => $row['20'],
+//                'type' => $row['23'],
+//                'user_id' => auth()->id(),
+//            ]);
+//        }
+//
+//        // 9 رقم المدرب
+//        //10 رقم البرنامج
+//        \App\Models\Batch::where("id", "!=", 0)->delete();
+//        for ($i = 1; $i <= 1191; $i++) {
+//            $row = $excelSpreadSheetData[0][$i];
+//
+//            $excelDate = $row[0];
+//            if (!is_numeric($excelDate)) {
+//                dd($row);
+//            }
+//
+//            $unixTimestamp = ($excelDate - 25569) * 86400; // تحويل إلى الطابع الزمني
+//            $date = date('Y-m-d', $unixTimestamp);
+//
+//            $count = \App\Models\Batch::where('id', $row[11])->count();
+//            if ($count == 0) {
+//                \App\Models\Batch::create([
+//                    'id' => $row[11],
+//                    'trainer_id' => $row[9],
+//                    'course_id' => $row[10],
+//                    'start_date' => $date,
+//                    'end_date' => $date,
+//                    'completed' => true,
+//                    'user_id' => auth()->id(),
+//                ]);
+//            }
+//        }
+//        $id = 1;
+//        \App\Models\Student::where("id", "!=", 0)->delete();
+//        for ($i = 1; $i <= 1191; $i++) {
+//            $row = $excelSpreadSheetData[0][$i];
+//
+//            $excelDate = $row[0];
+//
+//            $unixTimestamp = ($excelDate - 25569) * 86400; // تحويل إلى الطابع الزمني
+//            $date = date('Y-m-d', $unixTimestamp);
+//
+//            $count = \App\Models\Student::where('arabic_name', $row[3])->count();
+//            if ($count == 0) {
+//                \App\Models\Student::create([
+//                    'id' => $id,
+//                    'arabic_name' => $row[3],
+//                    'phone' => $row[4],
+//                    'user_id' => auth()->id(),
+//                ]);
+//                $id++;
+//            }
+//        }
+//
+//        $id = 1;
+//        \App\Models\BatchStudent::where("id", "!=", 0)->delete();
+//        for ($i = 1; $i <= 1191; $i++) {
+//            $row = $excelSpreadSheetData[0][$i];
+//
+//            $excelDate = $row[0];
+//
+//            $unixTimestamp = ($excelDate - 25569) * 86400; // تحويل إلى الطابع الزمني
+//            $date = date('Y-m-d', $unixTimestamp);
+//
+//            $student = \App\Models\Student::where('arabic_name', $row[3])->first();
+//
+//            if ($student) {
+//                \App\Models\BatchStudent::create([
+//                    'id' => $id,
+//                    'student_number' => $row[1],
+//                    'student_id' => $student->id,
+//                    'batch_id' => $row[11],
+//                    'want_certification' => $row[2] != null ? true : false,
+//                    'certification_id' => $row[2] != null ? $row[2] : null,
+//                    'date' => $date,
+//                    'user_id' => auth()->id(),
+//                ]);
+//                $id++;
+//
+//            } else {
+//                dd($row);
+//            }
+//        }
+//
+//
         $filePath = public_path('expenses.xlsx');
 
         $excelSpreadSheetData = Excel::toCollection(null, $filePath);
