432f9b1daf1ce7b117c69d3946a44732504afc41
ali3amer <aliaamer549@gmail.com>
2025-07-09 13:15:04 +0300

    last update day 09-07-2025

--- resources/views/livewire/expense.blade.php
+++ resources/views/livewire/expense.blade.php
@@ -15,7 +15,8 @@
                     <x-select name="bank_id" :disabled="$payment_method == 'cash' || ($payment_method == 'bank' && empty($banks))" :options="$banks" label="البنك"/>
                     <x-input name="transaction_id" :disabled="$payment_method == 'cash' || ($payment_method == 'bank' && empty($banks))" label="رقم الاشعار"/>
                     <div class="grid gap-x-1 grid-cols-2">
-                        <x-button model="expenses" :disabled="floatval($amount) == 0 || (floatval($amount) > session($payment_method.'_balance'))" type="submit" label="حفظ"/>
+{{--                        <x-button model="expenses" :disabled="floatval($amount) == 0 || (floatval($amount) > session($payment_method.'_balance'))" type="submit" label="حفظ"/>--}}
+                        <x-button model="expenses" :disabled="floatval($amount) == 0" type="submit" label="حفظ"/>
                         <x-button model="expenseOptions" permission="read" wire:click="$toggle('optionMode')" class="bg-yellow-500"
                                   label="التصنيفات"/>
                     </div>
